80e83b0a4d0fbbe130181af78ee2100e1ac2df01
sumit chahar <chahar.sumit888@gmail.com>
2021-09-20 12:49:26 +0530

    Complete people of GOT app

--- block-BJaaew/code/script.js
+++ block-BJaaew/code/script.js
@@ -1,50 +1,65 @@
-function main(){
+  let root = document.querySelector('.container');
   let ul = document.querySelector('ul');
-  let filterPeople = document.querySelector('.filter');
-  let btn = document.querySelectorAll('.btn');
-  let activeHouse = "";
-  function handleBtnUI(event){
-    event.target.classList.add("selected");
-    createUI();
-    activeHouse = event.target;
-  }
+  let search = document.querySelector('.search');
+  let allHouses = got.houses.map(house => house.name);
+  let rootHouses = document.querySelector('.filter');
+  
+  let allPeople = got.houses.reduce((acc,curr) => {
+    acc = acc.concat(curr.people);
+    return acc;
+  }, []);
 
-  function createUI(){
-    got.houses.forEach(elm => {
-      for(people of elm.people){
-        let li = document.createElement('li');
-        li.classList.add("character-list-item","flex-col");
-        let div = document.createElement('div');
-        div.classList.add("flex");
-        let img = document.createElement('img');
-        img.src = people.image;
-        let h3 = document.createElement('h3');
-        h3.innerText = people.name;
-        h3.classList.add("text-lg");
-        let p = document.createElement('p');
-        p.innerText = people.description;
-        p.classList.add("text-sm");
-        let button = document.createElement('button');
-        button.innerText = "Learn More!";
-        button.classList.add("btn","text-md");
-        div.append(img,h3);
-        li.append(div,p,button);
-        ul.append(li);
+  let activeHouse = "";
+  function createHouseUI(allHouses = []){
+    rootHouses.innerHTML = "";
+    allHouses.forEach((house) => {
+      let btn = document.createElement("button");
+      btn.classList.add("btn");
+      btn.innerText = house;
+      if(activeHouse === house){
+        btn.classList.add('selected');
       }
+      btn.addEventListener('click', () => {
+        activeHouse = house;
+        let peopleOfHouse =  got.houses.find(currHouse => currHouse.name === house).people || [];
+        createUI(peopleOfHouse);
+        createHouseUI(allHouses);
+      });
+      rootHouses.append(btn);
     });
   }
 
-
-  function updateDefaultButton(btn = activeHouse){
-    btn.classList.remove("selected");
-    activeHouse.classList.add("selected");
+  function createUI(data = []){
+    ul.innerHTML = "";
+    data.forEach(people => {
+      let li = document.createElement('li');
+      li.classList.add("character-list-item","flex-col");
+      let div = document.createElement('div');
+      div.classList.add("flex");
+      let img = document.createElement('img');
+      img.src = people.image;
+      let h3 = document.createElement('h3');
+      h3.innerText = people.name;
+      h3.classList.add("text-lg");
+      let p = document.createElement('p');
+      p.innerText = people.description;
+      p.classList.add("text-sm");
+      let button = document.createElement('button');
+      button.innerText = "Learn More!";
+      button.classList.add("btn","text-md");
+      div.append(img,h3);
+      li.append(div,p,button);
+      ul.append(li);
+    });
   }
-
-  updateDefaultButton();
-
-  btn.addEventListener('click', handleBtnUI);
-}
-main();
+  function handleSearch(event){
+    let searchText = event.target.value;
+    let filteredPeople = allPeople.filter(p => p.name.toLowerCase().includes(searchText.toLowerCase()));
+    createUI(filteredPeople);
+  }
+  search.addEventListener('input', handleSearch);
+  createUI(allPeople);
+  createHouseUI(allHouses);
 
 /*<li class="character-list-item">
           <div class="flex">
